41cc2cc6dfe1df29b7ab05783a2e2912456a49c7
Matt Godfrey <godfrey.m1992@gmail.com>
2020-08-26 16:49:34 -0400

    adding .gitignore

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/", function (req, res) {
+  console.log(req.header)
 });
 
 
